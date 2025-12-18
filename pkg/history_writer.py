68f313f540403896dc223eb14499383d9eeb1570
root <you@example.com>
2025-12-18 15:49:43 +0000

    ddpm train

--- pkg/history_writer.py
+++ pkg/history_writer.py
@@ -1,3 +1,4 @@
+import torch
 from matplotlib import pyplot as plt
 
 
@@ -8,12 +9,22 @@ class HistoryWriter():
     def save_record(self, key, value):
         if key not in self.history:
             self.history[key] = []
+
+        # сразу сохраняем CPU-float
+        if torch.is_tensor(value):
+            value = value.detach().cpu().item()
+
         self.history[key].append(value)
 
     def plot_history(self, x_label, save_path):
         for key, values in self.history.items():
             fig, ax = plt.subplots()
-            ax.plot(values)
+            clean_values = [
+                v.detach().cpu().item() if torch.is_tensor(v) else float(v)
+                for v in values
+            ]
+
+            ax.plot(clean_values)
             ax.set_xlabel(x_label)
             ax.set_ylabel(key)
             fig.savefig(f"{save_path}_{key}.png")
